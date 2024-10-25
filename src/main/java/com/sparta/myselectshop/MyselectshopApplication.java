6f2ee2cd3221d311426ce7078fbba0753d83c61f
hizdev <gpfksdlrn@naver.com>
2024-10-25 23:05:35 +0900

    Scheduler 기능 구현

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -3,7 +3,9 @@ package com.sparta.myselectshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing // Spring Data Jpa 에서 시간에 대한 값을 자동으로 넣어주는 기능
 @SpringBootApplication
 public class MyselectshopApplication {
